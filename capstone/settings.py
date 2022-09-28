ce35f32e92c2f4ea02db9fa4cf6b22581c7c0dc0
Siddhant Sharma <86948743+Sidd2024@users.noreply.github.com>
2022-09-28 10:29:05 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'capstone.urls'
