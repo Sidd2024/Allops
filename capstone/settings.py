c59aa4a515c077e2e6afe9b58fbc495f47a19d48
Siddhant Sharma <86948743+Sidd2024@users.noreply.github.com>
2022-09-28 13:07:59 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -118,8 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
