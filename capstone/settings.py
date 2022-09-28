b267aab0beb722d503a0591d4119c9fc151a7275
Siddhant Sharma <86948743+Sidd2024@users.noreply.github.com>
2022-09-28 21:49:44 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$9wq_#gdunl%k9vhpiwri2%&r^=re6(52$yskubrj=%b@5e_)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
