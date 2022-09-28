acf44efa357cf21ee8ffa2489a0e2e0734802400
Siddhant Sharma <86948743+Sidd2024@users.noreply.github.com>
2022-09-28 14:34:17 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
