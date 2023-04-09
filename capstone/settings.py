31a4d5ec48a713bd099bad9e81299d40a1ba2559
Siddhant Sharma <86948743+Sidd2024@users.noreply.github.com>
2023-04-09 11:30:05 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -139,9 +141,6 @@ EMAIL_USE_TLS = True
 
 #heroku url configuration
 ALLOWED_HOSTS = [
-    '*',
-    '0.0.0.0',
-    'allops.herokuapp.com',
-    '127.0.0.1'
+    '*'
 ]
 
